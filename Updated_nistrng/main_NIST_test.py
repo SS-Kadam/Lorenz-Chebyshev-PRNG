6b7ab3c399bf3547e2f1abef83615d210c4632a2
SS-Kadam <shardulkadam706@gmail.com>
2026-03-15 22:36:09 +0530

    Updated main_NIST_test.py

--- Updated_nistrng/main_NIST_test.py
+++ Updated_nistrng/main_NIST_test.py
@@ -22,7 +22,7 @@ from nistrng.sp800_22r1a.test_cumulative_sums import CumulativeSumsTest
 # ----------------------------------------------------------------------
 # CONFIG AREA
 # ----------------------------------------------------------------------
-FILE_PATH = r"C:\Users\Square Panda Admin\PycharmProjects\HelloWorld\app.py\GitHub _10 mil.bin"
+FILE_PATH = r"C:\Users\Square Panda Admin\PycharmProjects\HelloWorld\app.py\file_name"
 TEST_TO_RUN = "monobit"
 
 BLOCK_SIZE = 128
